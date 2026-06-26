475e8bcb08b473f26e4067f49ea67335b711f829
Patrick Wang <89101659+MisterKrabz@users.noreply.github.com>
2026-06-26 17:50:20 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 19.3 MB (92.39%)

--- 0078-subsets/solution.py
+++ 0078-subsets/solution.py
@@ -1,22 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ret = list()
-        current = list()
+        ret = []
 
-        def recurse(i): 
+        def backtrack(i, current): 
             if i == len(nums): 
                 ret.append(current[:])
                 return 
-
-            recurse(i + 1)
-
+            
             current.append(nums[i])
-            recurse(i + 1)
-
+            backtrack(i + 1, current)
             current.pop()
-        
-        recurse(0)
-        return ret
 
+            backtrack(i + 1, current)
+        
+        backtrack(0, [])
+        return ret 
 
 
