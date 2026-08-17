0f41d877649ecfd82719493c91ea0b80a60a996b
Patrick Wang <89101659+MisterKrabz@users.noreply.github.com>
2026-08-17 23:34:41 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 19.2 MB (99.67%)

---
+++ 0103-binary-tree-zigzag-level-order-traversal/solution.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+class Solution:
+    def zigzagLevelOrder(self, root):
+        if not root:
+            return []
+
+        queue = deque([root])
+        result = []
+        left_to_right = True
+
+        while queue:
+            level = []
+            level_size = len(queue)
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+            if not left_to_right:
+                level.reverse()
+
+            result.append(level)
+            left_to_right = not left_to_right
+
+        return result
